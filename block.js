09ae0a39404ec701836237a10d6f1ba1e70e08d7
nguyễn hòa <92292886+NguyenHoaFsoft@users.noreply.github.com>
2024-12-22 17:17:17 +0700

    add gesnis block

--- block.js
+++ block.js
@@ -1,9 +1,15 @@
+import GENESIS_DATA from './config.js';
+
 class Block {
     constructor({ timestamp, lastHash, hash, data }) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+    }
+
+    static genesis() {
+        return new this(GENESIS_DATA);
     }
 }
 export default Block;
