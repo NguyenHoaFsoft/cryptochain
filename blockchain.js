83fdc22f355b88316734e7368eb2152048fdc6bb
nguyễn hòa <92292886+NguyenHoaFsoft@users.noreply.github.com>
2025-01-04 00:08:11 +0700

    update

--- blockchain.js
+++ blockchain.js
@@ -29,21 +29,7 @@ class Blockchain {
         console.log('Replacing chain with', chain);
         this.chain = chain;
     }
-    // static isValidChain(chain) {
-    //     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
-    //         return false;
-    //     }
 
-    //     for (let i = 1; i < chain.length; i++) {
-    //         const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
-    //         const actualLastHash = chain[i - 1].hash;
-    //         if (lastHash !== actualLastHash) return false;
-
-    //         const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-    //         if (hash !== validatedHash) return false;
-    //     }
-    //     return true;
-    // }
     static isValidChain(chain) {
         if (!chain.length) return false;
 
@@ -79,4 +65,4 @@ class Blockchain {
 }
 
 
-export default Blockchain; // Default export
+export default Blockchain;
